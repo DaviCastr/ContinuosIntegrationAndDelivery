0a54907cbd80a4dbe35b7dc8b45085581e3ce3ef
Davi Castro <davifgeo@gmail.com>
2024-09-23 15:56:59 +0000

    initial commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'travels/test/integration/FirstJourney',
+		'travels/test/integration/pages/TravelList',
+		'travels/test/integration/pages/TravelObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, TravelList, TravelObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('travels') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheTravelList: TravelList,
+					onTheTravelObjectPage: TravelObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
